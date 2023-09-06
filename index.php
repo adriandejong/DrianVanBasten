b4993b57d9d2c40981b47cf70ee6c20f9edb0853
adriandejong <syah100629@gmail.com>
2023-09-06 11:10:22 +0700

    halo

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+// include 'dbcontroller.php';
+require_once('dbcontroller.php');
+$db = new dbController();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+
+<body class="body">
+    <!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm mb-3">
+  <div class="container ">
+    <a class="navbar-brand" href="#">SMKN 40 Jakarta</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!-- Akhir Navbar -->
+
+<!-- Cards -->
+<div class="container">  
+ <div class="row text-center justify-content-center">
+
+ <?php
+    $sql = "select * from t_kelas";
+    $row = $db->getALL($sql);
+    foreach ($row as $value) :
+?> 
+
+<div class="col-md-4 mb-5">
+<div class="card " style="width: 18rem;">
+  <img src="img/<?php echo $value['f_nama'];?>.jpg" class="card-img-top" alt="jurusan">
+  <div class="card-body">
+    <h5 class="card-title">
+      
+    <?php echo $value['f_nama']; ?>
+  
+  </h5>
+    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic ex aperiam accusantium. Odit eos illo officia. Assumenda fuga, corrupti tenetur, natus rem vero porro pariatur magnam facilis doloribus enim expedita.
+    </p>
+    <a href="#" class="btn btn-primary">Selengkapnya</a>
+  </div>
+  </div>
+  </div>
+
+  <?php
+    endforeach
+?>
+</div>
+
+</div>
+
+<!-- Akhir Cards -->
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+</body>
+
+</html>
